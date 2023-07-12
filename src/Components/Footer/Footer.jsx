72ffc70d0bf44ef16c7edd15c51f1706c62104a2
Simon-Honore <simon.honore6@gmail.com>
2023-07-12 16:55:32 +0200

    feat(app): add footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+// librairies
+import classes from './Footer.module.css';
+
+function Footer() {
+  return (
+    <footer className={classes.Footer}>
+			2023 © Blog - Simon Honoré
+    </footer>
+  );
+};
+
+export default Footer;
--- src/Hoc/Layout/Layout.jsx
+++ src/Hoc/Layout/Layout.jsx
@@ -1,15 +1,19 @@
 // librairies
-import './Layout.module.css';
+import classes from'./Layout.module.css';
 
 // components
 import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
 
 function Layout({children}) {
   return (
-    <>
+    <div className={classes.Layout}>
       <Header />
-      {children}
-    </>
+      <main>
+        {children}
+      </main>
+      <Footer />
+    </div>
   );
 };
 
